7ca8a402921b6d3f6e78074b37b7ad9da2bb95be
adam <adamnatanpassaretti@gmail.com>
2024-06-23 07:45:33 +0200

    stable HTML integration

---
+++ app/src/main/java/com/example/webviewintegration/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.webviewintegration;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    // WebView object reference
+    WebView webView;
+    WebSettings webSettings;
+
+    // Function triggered on app creation
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        webView = findViewById(R.id.webView);
+        webSettings = webView.getSettings();
+        // For zooming control integration
+        webSettings.setBuiltInZoomControls(true);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        // Web View client handles the logic that permits to
+        // navigate through the various pages of the web app
+        webView.setWebViewClient(new Callback());
+
+        // For online URL rendering
+        //webView.loadUrl("https://www.youtube.com/")
+
+        // Loading local index file
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // Im telling to android to not override and let webView load
+            return false;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
